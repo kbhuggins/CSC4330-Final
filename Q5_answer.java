dabb34b587e80721af3b42a1b4c3433137075c48
Kahrin Huggins <kahrinhuggins@gmail.com>
2020-05-03 23:12:35 -0400

    Add files via upload

---
+++ Q5_answer.java
@@ -0,0 +1,19 @@
+//Kahrin Huggins CSC4330 Q5
+public class Q5_answer {
+    public static void main(String[] args){
+        int n;
+
+        for (int i =1; i<=n; i++){
+            reject:
+                for (int j=1; j<=n;j++){
+                    if(x[i][j] != 0){
+                        break reject;
+                    }
+                 System.out.println("First all-zero row is:" + i);
+                  //statements
+                }
+
+        }
+
+    }
+}
